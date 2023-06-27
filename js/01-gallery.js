17a642ada39984540b585d4e86406c7e9bdfd07d
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-06-27 17:35:57 +0300

    well_done)
    1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryContainerRef = document.querySelector(".gallery");
 
 galleryContainerRef.addEventListener("click", showOriginalImg);
+
+function createGalleryElements() {
+  const galleryElements = galleryItems
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+    )
+    .join("");
+  galleryContainerRef.insertAdjacentHTML("beforeend", galleryElements);
+}
 
 createGalleryElements();
 
@@ -28,21 +44,4 @@ function showOriginalImg(event) {
       //Цікаво запитати в Руслана!!!!!
     }
   }
-}
-function createGalleryElements() {
-  const galleryElements = galleryItems
-    .map(
-      ({ preview, original, description }) => `<li class="gallery__item">
-  <a class="gallery__link" href=${original}>
-    <img
-      class="gallery__image"
-      src=${preview}
-      data-source=${original}
-      alt=${description}
-    />
-  </a>
-</li>`
-    )
-    .join("");
-  galleryContainerRef.insertAdjacentHTML("beforeend", galleryElements);
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(".gallery");
+
+function createGalleryElements() {
+  const galleryElements = galleryItems
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>
+</li>`
+    )
+    .join("");
+  galleryContainerRef.insertAdjacentHTML("beforeend", galleryElements);
+}
+createGalleryElements();
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+  //   ===========
+});
